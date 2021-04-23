9ebecc227025620c5a9a19010c59fdd1cab76de0
AngelSharma-hash <81636976+AngelSharma-hash@users.noreply.github.com>
2021-04-23 18:31:54 +0530

    Add files via upload

---
+++ rect.js
@@ -0,0 +1,22 @@
+class Box{
+    constructor(x,y,width,height){
+
+        var options = {
+            restitution:0
+        }
+
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.width=width;
+        this.height = height;
+       
+    }
+    display(){
+        var pos = this.body.position;
+    
+        rectMode(CENTER);
+        
+        rect(pos.x,pos.y,this.width,this.height);
+    }
+   
+}
+
